a37b9e01fc1db1a642914ceb0303df2fe44dc7b0
pmarlowe791321 <791321@seq.org>
2018-09-28 10:50:49 -0700

    progress

--- SnakeLab/main.js
+++ SnakeLab/main.js
@@ -4,6 +4,8 @@ window.onload = init;//  After the window has been loaded, go to init
 var canvas;
 var ctx;
 var boid;
+var segment;
+var snake;
 function init(){
   //get the canvas
   canvas = document.getElementById('cnv');
@@ -15,6 +17,8 @@ function init(){
   // get the context
   ctx = canvas.getContext('2d'); // This is the context
   boid = new Boid();
+  segment = new Segment();
+  snake = new Snake(segment);
   animate();
 }
 
@@ -25,6 +29,7 @@ function animate(){
   //for(let i = 0; i < balls.length; i++){
     //balls[i].run();
   //}
-  //boid.run();
+  boid.run();
   //orbiter.run();
+  snake.run();
 }
--- SnakeLab/snake.js
+++ SnakeLab/snake.js
@@ -1,26 +1,49 @@
 function Segment(){
-  var segrad = 10;
-  var loc = new JSVector();
+  this.segrad = 10;
+  this.loc = new JSVector();
 }
-function Snake(Segment){
-  var seglist = [];
-  var segnum = 6;
-  var seg0 = new segment();
-  seglist.push(seg0);
-  for (var i = 1; i < segnum, i++) {
-    var temp = new segment();
-    temp.loc.x = seglist[i-1].segrad*2;
-    temp.loc.y = seglist[i-1].segrad*2;
-    seglist.push(temp);
+function Snake(segment){
+  this.seglist = [];
+  this.segdist = segment.segrad*2;
+  this.segnum = 6;
+  this.segment = segment;
+  var seg0 = new Segment();
+  this.seglist.push(seg0);
+  for (var i = 1; i < this.segnum; i++) {
+    var temp = new Segment();
+    temp.loc.x = this.seglist[i-1].segrad*2;
+    temp.loc.y = this.seglist[i-1].segrad*2;
+    this.seglist.push(temp);
   }
-snake.prototype.run = function(){
-  this.render();
+}
+Snake.prototype.run = function(){
   this.update();
+  this.render();
 }
-snake.prototype.render = function(){
+Snake.prototype.render = function(){
+  for(var i=0;i<=this.segnum;i++){
+    ctx.strokeStyle = 'rgba(55,50,220)';
+    ctx.fillStyle = "rgba(100,162,120)";
+    ctx.beginPath();
+    ctx.arc(this.seglist[i].loc.x,this.seglist[i].loc.y, segment.segrad, 0, Math.PI*2, false);
+    ctx.stroke();
+    ctx.fill();
+  }
+}
+Snake.prototype.update = function(){
+  console.log(this.seglist[0].loc);
+  this.move(boid.loc,this.seglist[0].loc);
+  for (var i=1;i<this.segnum;i++){
+    this.move(this.seglist[i-1],this.seglist[i]);
+  }
+}
 
-}
-snake.prototype.update = function(){
-  var temp = JSVector.subGetNew(seglist[0],boid.loc);
-  seglist[0].loc.JSVector.add(seglist[0].loc,temp);
+Snake.prototype.move = function(targetloc, moverloc){
+  var temp = JSVector.subGetNew(targetloc.loc,moverloc.loc);
+  temp = temp.add(targetloc.loc);
+  temp.JSVector.setMagnitude(this.segdist);
+  moverloc.loc = temp;
+  // var temp = JSVector.subGetNew(boid.loc,seglist[0].loc);
+  // temp = JSVector.add(temp,boid.loc);
+  // seglist[0].loc = temp;
 }
