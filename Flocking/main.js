ddc96a33830a32e5796b21af8cd3dec11a32063c
pmarlowe791321 <791321@seq.org>
2018-10-26 10:35:44 -0700

    progress on adding click to create

---
+++ Flocking/main.js
@@ -0,0 +1,38 @@
+window.onload = init;//  After the window has been loaded, go to init
+
+// global variables for canvas and context
+var canvas;
+var ctx;
+var particleSystems;
+function init(){
+  //get the canvas
+  canvas = document.getElementById('cnv');
+  // Set the dimensions of the canvas
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+  canvas.style.border = 'solid black 5px';
+  canvas.style.backgroundColor = 'rgba(255,255,255, .9)';
+  canvas.addEventListener("click",handleMouseClick);
+  // get the context
+  ctx = canvas.getContext('2d'); // This is the context
+  particleSystems = [];
+
+
+  animate();
+}
+
+function animate(){
+
+  requestAnimationFrame(animate);
+  ctx.clearRect(0,0,window.innerWidth, window.innerHeight);
+
+  for(var i=0;i<particleSystems.length;i++){
+    particleSystems[i].run();
+  }
+
+}
+
+  function handleMouseClick(event){
+    particleSystems.push(new ParticleSys(event.clientX,event.clientY));
+  }
+}
---
+++ Flocking/particle.js
@@ -0,0 +1,96 @@
+function Particle(loc, vel, acc, base, height, lifespan){
+  this.loc = loc;
+  this.vel = vel;
+  this.acc = acc;
+  this.base = base;
+  this.height = height;
+  this.lifespan = lifespan;
+}
+
+function ParticleSys(x,y){
+  this.numParticles = 20;
+  this.particles = [];
+  this.loc = new JSVector(x,y);
+  for(var i = 0;i<this.numParticles;i++){
+    this.addParticle();
+  }
+}
+
+ParticleSys.prototype.run() = function(){
+  for(var i=0;i<this.particles.length;i++){
+    this.particles[i].run();
+  }
+  for(var i=0;i<this.particles.length;i++){
+    this.healthcheck(i);
+      }
+  }
+
+
+Particle.prototype.run = function(){
+  //this.checkEdges();
+  this.update();
+}
+
+Particle.prototype.checkEdges = function(){
+  if(this.loc.x > window.innerWidth || this.loc.x < 0)  this.vel.x = -this.vel.x
+  if((this.loc.y > window.innerHeight || this.loc.y < 0 )&& this.lifespan > 200)  this.vel.y = -this.vel.y
+}
+
+Particle.prototype.update = function(){
+  this.loc.x += this.vel.x;
+  this.loc.y += this.vel.y;
+  // this.acc.x = Math.random()*2-1;
+  // this.acc.y = Math.random()*2-1;
+  this.vel.x += this.acc.x;
+  this.vel.y += this.acc.y;
+  this.vel.limit(10);
+  //this.acc.limit(5);
+  this.lifespan -= 1;
+  this.render();
+}
+
+
+Particle.prototype.render = function(){
+  ctx.strokeStyle = 'rgba(75,0,130)';
+  ctx.fillStyle = "rgba(75,0,130)";
+  ctx.save();
+  ctx.translate(this.loc.x,this.loc.y);
+  ctx.rotate(this.vel.getDirection());
+  ctx.beginPath();
+  ctx.moveTo(0-this.height/2,0-this.base/2);
+  ctx.lineTo(this.height/2,0);
+  ctx.lineTo(0-this.height/2,this.base/2);
+  //ctx.moveTo(-10,-10);
+  //ctx.lineTo(10,0);
+  //ctx.lineTo(-10,10);
+  //ctx.arc(this.loc.x,this.loc.y, this.rad, Math.PI*2, 0, false);
+  ctx.fill();
+  ctx.restore();
+}
+
+// Particle.prototype.healthcheck = function(){
+//   if (this.lifespan<1){
+//
+//   }
+// }
+ParticleSys.prototype.healthcheck = function(i){
+  if (this.particles[i].lifespan<1){
+    this.particles.splice(i, 1);
+    this.addParticle();
+  }
+ParticleSys.prototype.addParticle = function(){
+    //var x = Math.random()*window.innerWidth;
+    //var y = Math.random()*window.innerHeight;
+    var dx = Math.random()*2-1;
+    var dy = Math.random()*-2-1;
+    var vel = new JSVector(dx, dy);
+    var ax = 0;
+    var ay = 0.01;
+    var base = 30;
+    var height = 30;
+    var acc = new JSVector(ax, ay);
+    //var lifespan = Math.random()*1000;
+    // var lifespan = Math.random()*1500;
+    var lifespan = Math.random()*200+300;
+    this.particles.push(new Particle(this.loc, vel, acc, base, height, lifespan));
+}
